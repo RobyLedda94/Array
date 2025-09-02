8409a07e45962bc012193b745297b844939c40f1
RobyLedda94 <robyledda01@yahoo.it>
2025-09-02 11:50:57 +0200

    [UPD] trovo il valore massimo dell'array

--- js/script.js
+++ js/script.js
@@ -16,3 +16,23 @@ for (let i = 0; i < numbers_Array.length; i++) { // Definisco un ciclo for per s
 };
 
 console.log(`La somma tra i numeri è di : ${sum}`); // Stampo in console il risultato
+
+
+
+
+// Trovare il valore più grande nell'array
+
+let max = numbers_Array[0]; // Dichiaro una variabile a cui assegno il primo valore dell'array
+
+
+for (let i = 0; i < numbers_Array.length; i++) { // Definisco un ciclo for per scorrere tutti gli elementi dell'array
+
+    if (numbers_Array[i] > max) { // Condizione dove indico se l'elemento corrente è maggiore del valore iniziale di max
+
+        max = numbers_Array[i]; // Aggiorno il valore di max con il valore piu grande che trova
+
+    };
+
+};
+
+console.log(`${max}`); // Srampo in console il valore più grande
