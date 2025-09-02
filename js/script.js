30ad73b524f168a685129588cb08a55f6592649d
RobyLedda94 <robyledda01@yahoo.it>
2025-09-02 11:30:12 +0200

    [UPD] somma degli elementi dell'array

--- js/script.js
+++ js/script.js
@@ -2,4 +2,17 @@
 
 const numbers_Array = [5, 12, 8, 21, 7];
 
-console.log(`Array numerico :`, numbers_Array);
+console.log(`Array numerico :`, numbers_Array); // Stampo l'array in console
+
+
+// Somma degli elementi
+
+let sum = 0; // Dichiarazione variabile che tiene traccia della somma
+
+for (let i = 0; i < numbers_Array.length; i++) { // Definisco un ciclo for per scorrere tutti gli elementi dell'array
+
+    sum = sum + numbers_Array[i]; // Ad ogni iterazione aggiungo alla variabile somma, il valore dell'elemento corrente dell'array e lo sommo
+
+};
+
+console.log(`La somma tra i numeri è di : ${sum}`); // Stampo in console il risultato
