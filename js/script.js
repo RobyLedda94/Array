0360473dff5fea25d033005a204d3382ab02185e
RobyLedda94 <robyledda01@yahoo.it>
2025-09-02 11:13:41 +0200

    [UPD] stampo in console, l'array

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-console.log('Prova')
+// Creazione di un array di numeri, stamparlo in console, sommare tra loro gli elementi e trovare il massimo
+
+const numbers_Array = [5, 12, 8, 21, 7];
+
+console.log(`Array numerico :`, numbers_Array);
